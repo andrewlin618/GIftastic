acc292a0b868e50d423b5ae8bcff1ae9d0828d81
Andrew Lin <andrewlin618@gmail.com>
2019-09-11 10:52:28 -0700

    Update: buttons show here done

--- assets/Javascript/gif.js
+++ assets/Javascript/gif.js
@@ -1,28 +1,25 @@
+var animalBank = ["cat", "dog", "rabbit", "cow"];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(".btn-success").on("click",function(){
-alert("111");
+$(".btn-success").on("click", function () {
+    if ($("#add-animal").val() === "") {
+        return;
+    }
+    var name = $("#add-animal").val().toLowerCase();
+    if (animalBank.indexOf(name) >= 0) {
+        alert("You have this animal already");
+    } else {
+        animalBank.push(name);
+        var newButton = $("<button>");
+        newButton.addClass("btn btn-secondary m-1");
+        newButton.attr("animal-name", name);
+        newButton.text(name);
+        $("#buttons-show-here").append(newButton);
+    }
+    $("#add-animal").val('');
+    console.log(animalBank);
 });
 
-$(".btn-secondary").on("click",function(){
-    alert("111");
+
+$(".btn-secondary").on("click", function () {
+
 });
